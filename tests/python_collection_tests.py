128228541acdf5632cb49e0f482f2b59dfd49abe
beepscore <support@beepscore.com>
2018-09-21 21:29:54 -0700

    Add test_types

--- tests/python_collection_tests.py
+++ tests/python_collection_tests.py
@@ -16,6 +16,25 @@ class TestPythonCollections(unittest.TestCase):
         self.assertEqual(my_tuple[-1], 'c')
         self.assertEqual(my_tuple[:-1], ('a', 'b'))
 
+    def test_types(self):
+        """
+        Python doesn't have a notion of "primitive" types
+        everything is an object, including int and bool
+        A variable doesn't have a type, but its value does.
+        """
+        # bool
+        self.assertEqual(type(False), bool)
+
+        # integer has type int
+        self.assertEqual(type(3), int)
+
+        # float
+        self.assertEqual(type(3.14), float)
+
+        # dictionary has type dict
+        my_dict = {'a': 1, 'b': 5, 'd': -9}
+        self.assertEqual(type(my_dict), dict)
+
 
 if __name__ == '__main__':
     unittest.main()
