0df4acf7fcee6d71c834c7b7b9ccdbec4fd8a285
beepscore <support@beepscore.com>
2018-09-21 23:38:03 -0700

    Add "arbitrary number of"

--- arguments.py
+++ arguments.py
@@ -4,7 +4,7 @@
 def multiply_with_positional_args(*args):
     """
     https://stackoverflow.com/questions/36901/what-does-double-star-asterisk-and-star-asterisk-do-for-parameters
-    :param args: a tuple with positional arguments
+    :param args: a tuple with arbitrary number of positional arguments
     :return:
     """
     foo = args[0]
@@ -15,7 +15,7 @@ def multiply_with_positional_args(*args):
 def add_with_kwargs(**kwargs):
     """
     https://stackoverflow.com/questions/36901/what-does-double-star-asterisk-and-star-asterisk-do-for-parameters
-    :param kwargs: a dictionary with keyword arguments
+    :param kwargs: a dictionary with arbitrary number of keyword arguments
     :return:
     """
     # get is safer method, if not in dictionary returns None instead of KeyError
