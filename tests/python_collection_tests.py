767bc66fef83cc9966704cab7b6b3f35038cdb20
beepscore <support@beepscore.com>
2018-09-21 21:17:03 -0700

    Add tests/python_collection_tests.py

---
+++ tests/python_collection_tests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+class TestPythonCollections(unittest.TestCase):
+    """
+    This class isn't meant to test Python's methods, but check my understanding.
+    """
+
+    def test_tuple_slice(self):
+        """
+        slice works on tuple similar to list
+        """
+        my_tuple = ('a', 'b', 'c')
+        self.assertEqual(my_tuple[-1], 'c')
+
+
+if __name__ == '__main__':
+    unittest.main()
