9738439bc10af49b764a20c08da5fdd3c4004b52
beepscore <support@beepscore.com>
2018-09-23 00:08:20 -0700

    Add assertion

--- tests/test_attie.py
+++ tests/test_attie.py
@@ -20,6 +20,10 @@ class TestPythonCollections(unittest.TestCase):
         # gack! this changed attie.a_dict too!
         self.assertEqual(attie.a_dict, {'a': 3, 'c': 4})
 
+        # this assigns a new dictionary to sample_a_dict so doesn't affect attie.a_dict
+        sample_a_dict = {'k': 5}
+        self.assertEqual(attie.a_dict, {'a': 3, 'c': 4})
+
         # mutate sample_b_dict
         sample_b_dict['g'] = 17
         self.assertEqual(sample_b_dict, {'b': 14, 'g': 17})
