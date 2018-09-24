c39300d177f465384991d1123a00045e090c0a37
beepscore <support@beepscore.com>
2018-09-24 09:19:08 -0700

    Add tuple and namedtuple

--- tests/test_python_collection.py
+++ tests/test_python_collection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
+from collections import namedtuple
 
 
 class TestPythonCollections(unittest.TestCase):
@@ -27,6 +28,15 @@ class TestPythonCollections(unittest.TestCase):
         # str
         my_str = 'hi mom'
         self.assertEqual(type(my_str), str)
+
+        # tuple
+        self.assertEqual(type((1, 'a')), tuple)
+
+        # namedtuple
+        # https://stackoverflow.com/questions/2970608/what-are-named-tuples-in-python#2970722
+        Point = namedtuple('Point', 'x, y')
+        my_point = Point(3, 5)
+        self.assertEqual(type(my_point), Point)
 
         # list
         my_list = [1, 5, -9]
