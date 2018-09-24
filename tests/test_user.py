562fc946083170e533fc5a93f40b59524cd392fe
beepscore <support@beepscore.com>
2018-09-24 13:50:59 -0700

    Comment out print statement, show output

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,7 +9,25 @@ class TestUser(unittest.TestCase):
     def test_add_attribute(self):
 
         user = User()
-        print(help(user))
+
+        # print(help(user))
+        """
+        Help on User in module python_collections_play.user object:
+        class User(builtins.object)
+         |  simplest possible class
+         |  Python 3 class implicitly inherits from object
+         |  Therefore implicitly inherits object constructor __init__(self)
+         |  https://stackoverflow.com/questions/4015417/python-class-inherits-object
+         |  
+         |  Data descriptors defined here:
+         |  
+         |  __dict__
+         |      dictionary for instance variables (if defined)
+         |  
+         |  __weakref__
+         |      list of weak references to the object (if defined)
+        """
+
         self.assertEqual(user.__dict__, {})
 
         # code outside a class can add an attribute to the class
