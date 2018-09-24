a1eec866bd6eae46b0c8ea0583928344cfde2a3a
beepscore <support@beepscore.com>
2018-09-24 12:32:47 -0700

    Add test_str and move some tests from test_type into it

---
+++ tests/test_str.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+class TestStr(unittest.TestCase):
+    """
+    This class isn't meant to test Python's methods, but check my understanding.
+    """
+
+    def test_type(self):
+        # str
+        my_str = 'hi mom'
+        self.assertEqual(type(my_str), str)
+
+    def test_str_slice(self):
+        """
+        slice works on str similar to list
+        """
+        my_str = 'Slartibartfast'
+        self.assertEqual(my_str[-1], 't')
+        self.assertEqual(my_str[:-1], 'Slartibartfas')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_type.py
+++ tests/test_type.py
@@ -29,18 +29,6 @@ class TestType(unittest.TestCase):
         y = decimal.Decimal(5)
         self.assertEqual(type(y), decimal.Decimal)
 
-        # str
-        my_str = 'hi mom'
-        self.assertEqual(type(my_str), str)
-
-    def test_str_slice(self):
-        """
-        slice works on str similar to list
-        """
-        my_str = 'Slartibartfast'
-        self.assertEqual(my_str[-1], 't')
-        self.assertEqual(my_str[:-1], 'Slartibartfas')
-
 
 if __name__ == '__main__':
     unittest.main()
