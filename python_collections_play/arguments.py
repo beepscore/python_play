5d4ce69a329044bde1fd38d760bcb951958a14f2
beepscore <support@beepscore.com>
2018-09-25 16:32:02 -0700

    Add func_param_mutable_default_antipattern. Do not put a mutable object as the default value of a function parameter.
    https://codehabitude.com/2013/12/24/python-objects-mutable-vs-immutable/

--- python_collections_play/arguments.py
+++ python_collections_play/arguments.py
@@ -38,3 +38,31 @@ def sum_of_kwargs_values(**kwargs):
     :return: sum of values of all kwargs
     """
     return sum(kwargs.values())
+
+
+def func_param_mutable_default_antipattern(collection=[]):
+    """
+    Do not put a mutable object as the default value of a function parameter,
+    because the same object is reused on multiple calls.
+    https://codehabitude.com/2013/12/24/python-objects-mutable-vs-immutable/
+    :param collection: a mutable collection with an undesirable mutable default
+    :return: collection by appending 'thing'
+    """
+    collection.append('thing')
+    return collection
+
+
+def func_param_immutable_default_safer(collection=None):
+    """
+    :param collection: a collection. default None which is immutable and therefor safer.
+    In general avoid defaulting a parameter to a mutable object,
+    because the same object is reused on multiple calls.
+    https://codehabitude.com/2013/12/24/python-objects-mutable-vs-immutable/
+    :return: collection by appending 'thing'
+    """
+    if collection is None:
+        # safer pattern, instantiate a new mutable object inside the function, not as a parameter default.
+        collection = []
+
+    collection.append("thing")
+    return collection
--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -3,6 +3,7 @@
 import unittest
 from python_collections_play.arguments import multiply_with_positional_args
 from python_collections_play.arguments import cat_plus_dog, sum_of_kwargs_values
+from python_collections_play.arguments import func_param_mutable_default_antipattern, func_param_immutable_default_safer
 
 
 class TestArguments(unittest.TestCase):
@@ -22,6 +23,24 @@ class TestArguments(unittest.TestCase):
         self.assertEqual(sum_of_kwargs_values(**{'dog': 4}), 4)
         self.assertEqual(sum_of_kwargs_values(**{'dog': 4, 'cat': 3}), 7)
         self.assertEqual(sum_of_kwargs_values(**{'dog': 4, 'cat': 3, 'zebra': 9}), 16)
+
+    def test_func_param_with_mutable_default_antipattern(self):
+        """
+        calling func_param_with_mutable_default_antipattern reuses default collection
+        """
+        self.assertEqual(func_param_mutable_default_antipattern(), ['thing'])
+        self.assertEqual(func_param_mutable_default_antipattern(), ['thing', 'thing'])
+        self.assertEqual(func_param_mutable_default_antipattern(), ['thing', 'thing', 'thing'])
+
+        self.assertEqual(func_param_mutable_default_antipattern(['wild']), ['wild', 'thing'])
+
+    def test_func_param_immutable_default_safer(self):
+        """
+        calling func_param_immutable_default_safer doesn't reuse default collection
+        """
+        self.assertEqual(func_param_immutable_default_safer(), ['thing'])
+        self.assertEqual(func_param_immutable_default_safer(), ['thing'])
+        self.assertEqual(func_param_immutable_default_safer(), ['thing'])
 
 
 if __name__ == '__main__':
