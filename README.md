6f4ba2424efd925cb43ab03464a7409b629506b0
beepscore <support@beepscore.com>
2018-09-25 21:30:17 -0700

    In decorators.py add another example.

--- README.md
+++ README.md
@@ -6,9 +6,6 @@ Play with Python to learn it better.
 ## Python Objects: Mutable vs. Immutable
 https://codehabitude.com/2013/12/24/python-objects-mutable-vs-immutable/
 
-## decorators
-https://www.programiz.com/python-programming/decorator
-
 # Results
 See unit tests for expected behavior.
 
--- decorators.py
+++ decorators.py
@@ -3,6 +3,11 @@
 
 class Decorators:
     """
+    A decorator in Python is any callable Python object that is used to modify a function or a class.
+    A reference to a function "func" or a class "C" is passed to a decorator and the decorator returns a modified function or class.
+    The modified functions or classes usually contain calls to the original function "func" or class "C".
+    https://www.python-course.eu/python3_decorators.php
+
     a decorator is a callable that returns a callable
     https://www.programiz.com/python-programming/decorator
     """
@@ -27,3 +32,34 @@ class Decorators:
     # @make_pretty
     # def ordinary2():
     #     print('I am ordinary2')
+
+##############
+
+# outside of class Decorator
+# example from https://www.python-course.eu/python3_decorators.php
+
+
+def our_decorator(func):
+
+    def function_wrapper(x):
+        print('Before calling ' + func.__name__)
+        func(x)
+        print('After calling ' + func.__name__)
+
+    return function_wrapper
+
+
+@our_decorator
+def foo(x):
+    print('Hi, foo has been called with ' + str(x))
+
+
+if __name__ == '__main__':
+
+    foo('Hi')
+    """
+    Before calling foo
+    Hi, foo has been called with Hi
+    After calling foo
+    """
+
