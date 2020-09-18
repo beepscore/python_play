e15bf4ceb0c1d18503b39c7e6426ef3aff7361c3
beepscore <support@beepscore.com>
2020-09-18 07:56:30 -0700

    update test name to match method under test

--- tests/test_date_utility.py
+++ tests/test_date_utility.py
@@ -6,7 +6,7 @@ from python_play.date_utility import random_date_string_inferring_format
 
 class TestDateUtility(unittest.TestCase):
 
-    def test_date_to_string(self):
+    def test_random_date_string_inferring_format(self):
         actual = random_date_string_inferring_format(start='1990-01-01', end='2020-12-31', date_string='2019-06-14')
         self.assertEqual('foo', actual)
 
